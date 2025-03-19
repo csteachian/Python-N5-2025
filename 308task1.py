efa663a4bcb5a6e99e4f36052752a96901204ffa
Ian Simpson <ian@csteach.uk>
2025-03-19 15:39:19 +0000

    308task1

---
+++ 308task1.py
@@ -0,0 +1,15 @@
+
+list = []
+
+while True:
+    #Get user input
+    percentage = int(input("Enter Percentage"))
+
+    #Validate between 0 and 100
+    while percentage < 0 or percentage > 100:
+        print("Error, % must be between 0 and 100")
+        percentage = int(input("Please enter a valid percentage"))
+    # add score to list
+
+    list.append(percentage)
+    print(list)
